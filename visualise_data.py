ee00ad8975f524cbfb11bd846b1b4f3ae87f2e67
natha5 <ns539@canterbury.ac.uk>
2023-04-21 10:41:54 +0100

    <add> Python files for data visualisation with pygame

---
+++ visualise_data.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+# handle import of gan output here
+
+screen = pygame.display.set_mode(800,800)
+
+
+values = {
+    "neo" : 0,
+    "pha" : 0,
+    "H" : 0.013,
+    "diameter" : 3.68,
+    "albedo" : 0.000353,
+    "e" : 0.0003,
+    "a" : 0.0108,
+    "q" : 0.010034,
+    "i" : 0.414,
+    "om" : 0.315,
+    "w" : 0.2886,
+    "ad" : 0.0117,
+    "n" : 0.000839,
+    "tp_cal" : 79138.937500,
+    "per" : 6.600572,
+    "moid" : 0.006
+}
+
+
