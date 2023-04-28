d36c49d84357270be60cab861ad8488df801b709
natha5 <ns539@canterbury.ac.uk>
2023-04-28 23:22:50 +0100

    <mod> integrated both files to call each other

--- visualise_data.py
+++ visualise_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from pygame.locals import *
 
+import asteroid_gan
+
 pygame.init()
 
 # handle import of gan output here
@@ -12,24 +14,9 @@ SCREEN_HEIGHT = 500
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
 
-values = {
-    "neo": 0,
-    "pha": 0,
-    "H": 0.013,
-    "diameter": 3.68,
-    "albedo": 0.000353,
-    "e": 0.0003,
-    "a": 0.0108,
-    "q": 0.010034,
-    "i": 0.414,
-    "om": 0.315,
-    "w": 0.2886,
-    "ad": 0.0117,
-    "n": 0.000839,
-    "tp_cal": 79138.937500,
-    "per": 6.600572,
-    "moid": 0.006
-}
+
+values = asteroid_gan.run_program()
+
 
 bg_img = pygame.image.load("resources/stars-sky-night.jpg")
 bg_img = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_WIDTH))
